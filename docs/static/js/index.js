c35075414e93dbcd08784f8ffa1546df825548fe
wefashe <wefashe@qq.com>
2024-04-01 14:53:42 +0800

    标题只显示两行，超出部分用省略号

--- docs/static/js/index.js
+++ docs/static/js/index.js
@@ -98,25 +98,28 @@ function setImage(db, pageIndex, pageSize){
   var stmt = db.prepare("select * from wallpaper w  order by enddate desc limit ($pageIndex * $pageSize), $pageSize");
   stmt.bind({ $pageIndex: pageIndex, $pageSize: pageSize });
   var image_list = document.getElementById('image-list')
+  var prefix = 'https://cn.bing.com'
   while (stmt.step()) {
       var row = stmt.getAsObject();  
-      var small_img_url = `https://cn.bing.com${row.url.substring(0,row.url.indexOf('&')) + '&w=120'}`   
-      var big_img_url = `https://cn.bing.com${row.url.substring(0,row.url.indexOf('&')) + '&w=384&h=216'}`   
+      var url = row.url.substring(0,row.url.indexOf('&'));
+      var small_img_url = `${prefix}${url}&w=120`;
+      var big_img_url = `${prefix}${url}&w=384&h=216`   
+      var view_count = Math.floor(Math.random()*(100 - 1000) + 1000);
       // 渐进式图片
       var image_html = `<div class="w3-quarter w3-padding"> 
                           <div class="w3-card w3-round me-card">
                             <div class="me-img">
-                              <a href = "https://cn.bing.com${row.copyrightlink}" target="_blank"> 
-                                <img id="${row.enddate}" class="me-img w3-image" src="${small_img_url}" data-src="${big_img_url}"  title="${row.copyright}" alt="https://cn.bing.com${row.urlbase}" loading="lazy" style="width:100%;max-width:100%"> 
+                              <a href = "${prefix}${row.copyrightlink}" target="_blank"> 
+                                <img id="${row.enddate}" class="me-img w3-image" src="${small_img_url}" data-src="${big_img_url}"  title="${row.copyright}" alt="${prefix}${row.urlbase}" loading="lazy" style="width:100%;max-width:100%"> 
                               </a> 
                             </div>
                             <div class = "w3-padding-small">
                               <div class="w3-row w3-padding-small w3-tiny"><i class="fa fa-instagram fa-lg fa-fw" style="color: orange; font-weight: bold;"></i> 必应美图</div>
-                              <div class="w3-row w3-padding-small me-img-title">${row.title}</div>
+                              <div class="w3-row w3-padding-small me-img-title" title="${row.title}">${row.title}</div>
                               <div class="w3-row w3-padding-small w3-small me-meta">
                                 <div class="w3-left">${row.enddate.replace(/^(\d{4})(\d{2})(\d{2})$/, "$1-$2-$3")}</div>
-                                <div class="w3-right" style="margin-left:12px"><i class="fa fa-heart"></i> ${Math.floor(Math.random()*(100 - 1000) + 1000)}</div>
-                                <div class="w3-right"><i class="fa fa-eye"></i> ${Math.floor(Math.random()*(100 - 1000) + 1000)}</div>
+                                <div class="w3-right" style="margin-left:12px"><i class="fa fa-heart"></i> ${view_count}</div>
+                                <div class="w3-right"><i class="fa fa-eye"></i> ${Math.floor(Math.random()*(view_count - 1000) + 1000)}</div>
                               </div>
                             </div>
                           </div>
